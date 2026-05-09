d19e781384a8fb1bc10a571a8dfd43a0d9c27f47
Jossu Ortiz <168014672+NW08@users.noreply.github.com>
2026-05-08 23:05:48 -0500

    Refactor Launcher to use PunchRepository and improve service initialization

--- src/Clase_05/TCP/Server/Launcher.java
+++ src/Clase_05/TCP/Server/Launcher.java
@@ -1,13 +1,16 @@
 package Clase_05.TCP.Server;
 
-import Clase_05.TCP.Server.services.ArrivalRegistryService;
+import Clase_05.TCP.Server.core.ArrivalRegistryService;
+import Clase_05.TCP.Server.repository.PunchRepository;
 
 public class Launcher {
-   void main() {
-      int port = 5412;
-      var registryService = new ArrivalRegistryService();
-      var server = new ServerApp(port, registryService);
+   private static final int PORT = 5412;
+   PunchRepository repository = new PunchRepository();
+   ArrivalRegistryService registryService = new ArrivalRegistryService(repository);
+   ServerApp server = new ServerApp(PORT, registryService);
 
+   void main() {
+      Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
       server.start();
    }
 }
