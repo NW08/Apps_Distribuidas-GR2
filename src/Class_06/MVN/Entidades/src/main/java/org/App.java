be82240642f55afac0b060aace9e3a57e81974fb
Jossu Ortiz <168014672+NW08@users.noreply.github.com>
2026-05-13 11:47:59 -0500

    Add entry for TCP Server project in README

---
+++ src/Class_06/MVN/Entidades/src/main/java/org/App.java
@@ -0,0 +1,10 @@
+package Class_06.MVN.Entidades.src.main.java.org;
+
+public class App {
+   static void main() {
+      Persona persona = new Persona("1234567890", "Juan", "Perez");
+      System.out.println(persona.getCedula());
+      System.out.println(persona.getNombre());
+      System.out.println(persona.getApellido());
+   }
+}
